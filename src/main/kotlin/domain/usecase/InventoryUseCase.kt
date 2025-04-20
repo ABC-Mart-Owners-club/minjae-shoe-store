0de6c3e9b78414380fb51cc0026f37c597b98f07
민재 <minjae@minjaeui-MacBookAir.local>
2025-04-20 09:28:15 +0900

    chore: Remove src directory from main branch

--- src/main/kotlin/domain/usecase/InventoryUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain
-
-import com.example.domain.TradeRepository
-
-class InventoryUseCase(private val tradeRepository: TradeRepository) {
-    fun checkInventory(id: String): Int {
-        return tradeRepository.checkInventory(id)
-    }
-}
